8967db6b66e980febb972ec9c422bd926cb3b328
tschudin <christian.tschudin@unibas.ch>
2021-09-05 08:51:49 +0200

    changing to 255.255.255.255 broadcast (like PatchWork), but it does not improve reliable detection

--- app/src/main/java/nz/scuttlebutt/tremola/ssb/peering/UDPbroadcast.kt
+++ app/src/main/java/nz/scuttlebutt/tremola/ssb/peering/UDPbroadcast.kt
@@ -1,22 +1,19 @@
 package nz.scuttlebutt.tremola.ssb.peering
 
 import android.content.Context
-import android.net.DhcpInfo
 import android.net.wifi.WifiManager
-import android.os.SystemClock
 import android.text.format.Formatter.formatIpAddress
 import android.util.Base64
 import android.util.Log
+import java.lang.Thread.sleep
+import java.net.DatagramPacket
+import java.net.InetAddress
+import java.util.concurrent.locks.ReentrantLock
+
 import nz.scuttlebutt.tremola.MainActivity
 import nz.scuttlebutt.tremola.WebAppInterface
 import nz.scuttlebutt.tremola.utils.Constants
 import nz.scuttlebutt.tremola.utils.Constants.Companion.UDP_BROADCAST_INTERVAL
-import java.lang.Thread.sleep
-import java.net.DatagramPacket
-import java.net.DatagramSocket
-import java.net.InetAddress
-import java.util.concurrent.locks.ReentrantLock
-
 
 class UDPbroadcast(val context: MainActivity, val wai: WebAppInterface?) {
 
@@ -27,13 +24,15 @@ class UDPbroadcast(val context: MainActivity, val wai: WebAppInterface?) {
 
         fun mkDgram(): DatagramPacket {
             val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
-            val dhcp = wifiManager.getDhcpInfo() // assumes IPv4?
+            // val dhcp = wifiManager.getDhcpInfo() // assumes IPv4?
+            /*
             val broadcast = dhcp.ipAddress or (0xff shl 24) // (-1 and dhcp.netmask.inv()) // dhcp.ipAddress and dhcp.netmask or dhcp.netmask.inv()
             val quads = ByteArray(4)
             for (k in 0..3)
                 quads[k] = (broadcast shr k * 8).toByte()
-            // val dest = InetAddress.getByName("255.255.255.255")
             val bcastAddr = InetAddress.getByAddress(quads)
+            */
+            val bcastAddr = InetAddress.getByName("255.255.255.255")
             val myAddr = wifiManager.connectionInfo.ipAddress
             // Log.d("UDP BEACON", "my=${formatIpAddress(myAddr)}, broadcast=${bcastAddr}, mask=${dhcp.netmask.inv()}")
             myMark = "net:${formatIpAddress(myAddr)}:${myTcpPort}~shs:" +
@@ -45,7 +44,7 @@ class UDPbroadcast(val context: MainActivity, val wai: WebAppInterface?) {
         while (true) {
             try {
                 val dgram = mkDgram()
-                val s = context.broadcast_socket
+                // val s = context.broadcast_socket
                 // Log.d("beacon sock", "bound=${s?.isBound}, ${s?.inetAddress}/${s?.port}/${s?.localPort} brcast${s?.broadcast}")
                 context.broadcast_socket!!.send(dgram)
                 Log.d("beacon", "sent") }
@@ -74,8 +73,9 @@ class UDPbroadcast(val context: MainActivity, val wai: WebAppInterface?) {
         val buf = ByteArray(256)
         val ingram = DatagramPacket(buf, buf.size)
         while (true) {
-            val s = context.broadcast_socket
-            // Log.d("listen", "${s}, ports=${s?.port}/${s?.localPort} closed=${s?.isClosed} bound=${s?.isBound}")
+
+            // val s = context.broadcast_socket
+            // Log.d("listen", "${s}, a=${s?.inetAddress}|${s?.localAddress}|${s?.localSocketAddress} br=${s?.broadcast}, ports=${s?.port}/${s?.localPort} closed=${s?.isClosed} bound=${s?.isBound} reuse=${s?.reuseAddress}")
             try { context.broadcast_socket!!.receive(ingram) }
             catch (e: Exception) {
                 // Log.d("Broadcast listen", "e=${e}, bsock=${context.broadcast_socket}")
