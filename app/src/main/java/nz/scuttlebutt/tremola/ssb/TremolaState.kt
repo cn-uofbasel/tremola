5da1f21eaaf5302e0eb516e456cc0736f765d848
Lars Waldvogel <lars.waldvogel@stud.unibas.ch>
2022-09-21 15:11:33 +0200

    Added documentation to TremolaState.kt

--- app/src/main/java/nz/scuttlebutt/tremola/ssb/TremolaState.kt
+++ app/src/main/java/nz/scuttlebutt/tremola/ssb/TremolaState.kt
@@ -21,52 +21,68 @@ import java.util.concurrent.ThreadFactory
 import java.util.concurrent.TimeUnit
 
 /**
- * All information about the local peer
+ * This class contains all the information about the local peer.
+ * @param context The app's context, MainActivity.
+ * @property numberOfCores The numbers of processing cores that are available.
+ * @property executorPool A pool of threads to do things asynchronously.
+ * @property db The database used for storing the relevant data.
+ * @property logDAO The data access object for the logs.
+ * @property pubDAO The data access object for the pubs.
+ * @property contactDAO The data access object for the contacts.
+ * @property idStore Contains the the cryptographic keys of the identity.
+ * @property peers The peers that are currently active or known.
+ * @property wai The WebAppInterface that the frontend uses to interact with the backend.
+ * @property msgTypes The SSBmsgTypes object.
  */
 class TremolaState(val context: Context) {
     private val numberOfCores = Runtime.getRuntime().availableProcessors()
-    private val executorPool: ScheduledExecutorService
+    private val executorPool: ScheduledExecutorService = Executors.newScheduledThreadPool(
+        numberOfCores,
+        threadFactory("SSB Pool", true)
+    )
 
-    // Load database
+    // Load database.
     private val db = TremolaDatabase.getInstance(context)
     val logDAO: LogEntryDAO = db.logDAO()
     val pubDAO: PubDAO = db.pubDAO()
     val contactDAO: ContactDAO = db.contactDAO()
 
-    // The identity, i.e. the crypto keys
+    // The identity, i.e. the crypto keys are stored in this field.
     val idStore = IdStore(context)
 
-    // The other known and/or active peers
-    val peers: PeeringPool
+    // The other known and/or active peers.
+    val peers: PeeringPool = PeeringPool(this)
 
-    // Interface to the frontend
+    // Interface to the frontend.
     lateinit var wai: WebAppInterface
     val msgTypes = SSBmsgTypes(this)
 
     init {
-        executorPool = Executors.newScheduledThreadPool(
-            numberOfCores,
-            threadFactory("SSB Pool", true)
-        )
-        peers = PeeringPool(this)
         addOwnIdentityAsFeed()
 
-        executorPool.scheduleAtFixedRate({ // connect to wiFi peers that show up
+        executorPool.scheduleAtFixedRate({ // Connect to Wi-Fi peers that show up.
             for (markOfAvailablePeers in (context as MainActivity).udp!!.markOfLocalPeers.keys) {
                 val s = markOfAvailablePeers.split("~")
-                // Transform public key into log ID
+                // Transform public key into log ID.
                 val logID = "@" + s[1].substring(4) + ".ed25519"
-                // 3 part address: 'net', ip address and port
+                // 3 part address: 'net', ip address and port.
                 val address = s[0].split(":")
-                peers.add(address[1], address[2].toInt(), logID) // will filter out already connected peers
+                // Will filter out already connected peers.
+                peers.add(address[1], address[2].toInt(), logID)
             }
         }, 3, WIFI_DISCOVERY_INTERVAL, TimeUnit.SECONDS)
-        executorPool.scheduleAtFixedRate({ // kick EBT (send current frontier) on regular intervals
+        executorPool.scheduleAtFixedRate({ // Kick EBT (send current frontier) at regular intervals.
             peers.kick()
         }, 5, EBT_FORCE_FRONTIER_INTERVAL, TimeUnit.SECONDS)
     }
 
     companion object {
+        /**
+         * This function returns a thread to execute a certain task.
+         * @param name The name of the ThreadFactory.
+         * @param daemon Whether the thread should be considered a daemon thread.
+         * @return A ThreadFactory that produces threads of the given name and daemon state.
+         */
         @JvmStatic
         fun threadFactory(name: String, daemon: Boolean): ThreadFactory {
             return ThreadFactory { runnable ->
@@ -77,6 +93,9 @@ class TremolaState(val context: Context) {
         }
     }
 
+    /**
+     * A function that adds all the people in all known pubs to the peers pool. Unused.
+     */
     private fun addPubsToPool() {
         executorPool.execute {
             val pubs = pubDAO.getAll()
@@ -86,25 +105,44 @@ class TremolaState(val context: Context) {
         }
     }
 
+    /**
+     * Updates the feed by kicking the Epidemic Broadcast Tree. Unused.
+     */
     fun syncFeeds() {
         executorPool.execute { peers.kick() }
     }
 
+    /**
+     * Adds a pub to the pub database and adds their peers to the pool.
+     * @param pub The pub to be added.
+     */
     fun addPub(pub: Pub) {
         executorPool.submit { pubDAO.insert(pub) }
         peers.add(pub.host, pub.port, pub.lid)
     }
 
+    /**
+     * Adds a LogEntry to the database.
+     * @param e The LogEntry to be added.
+     */
     fun addLogEntry(e: LogEntry) {
         executorPool.submit { logDAO.insert(e) }
     }
 
+    /**
+     * Adds a contact to the database.
+     * @param fid The friend ID to be added.
+     * @param alias The alias given by the user to the contact.
+     */
     fun addContact(fid: String, alias: String?) {
         contactDAO.insertContact(
             Contact(fid, alias, false, null, 1, 0, null)
         )
     }
 
+    /**
+     * Adds yourself to contact database.
+     */
     private fun addOwnIdentityAsFeed() {
         executorPool.submit {
             val lid = idStore.identity.toRef()
@@ -118,6 +156,9 @@ class TremolaState(val context: Context) {
         }
     }
 
+    /**
+     * Unused. TODO Remove or implement.
+     */
     fun processMessages() {
 /*
         Log.d("PROCESSING: ", "STARTED")
